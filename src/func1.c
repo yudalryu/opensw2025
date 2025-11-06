d3b1ce3db255586fc78156ae36371129aa3f8625
Jiwon <dbwldnjs5961@gmail.com>
2025-11-06 15:00:21 +0900

    new foofoo foofoofoo are defined

--- src/func1.c
+++ src/func1.c
@@ -3,3 +3,12 @@
 void foo(void){
     printf("Hello This is foo!\n");
 }
+
+void foofoo(void){
+    printf("Hello This is foo foo!\n");
+}
+
+void foofoofoo(void){
+    printf("Hello This is foo foo foo!\n");
+}
+
