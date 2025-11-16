83de7dee6ca505824f76bf4818780f8534b6a0df
Jiwon <dbwldnjs5961@gmail.com>
2025-11-17 00:27:36 +0900

    add makefile

--- week09_git/src/myapp.c
+++ week09_git/src/myapp.c
@@ -1,32 +1,21 @@
 #include <stdio.h>
 #include "myops.h"
 
-void calculate(int a, char* op, int b) {
+int calculate(int a, char op, int b) {
     int result;
 
-    if(op == '+') {
-        result = myadd(a, b);
-    } 
-    else if(op == '-') {
-        result = mysub(a, b);
-    }
-    else if(op == '*') {
-        result = mymul(a, b);
-    }
-    else if(op == '/') {
-        result = mydiv(a, b);
-    }
-    else if (op == '%') {
-        result = mymod(a, b);
-    }
-    else if (op == '^') { result = mypow(a, b);
-    }
+    if (op == '+') result = myadd(a, b);
+    else if (op == '-') result = mysub(a, b);
+    else if (op == '*') result = mymul(a, b);
+    else if (op == '/') result = mydiv(a, b);
+    else if (op == '%') result = mymod(a, b);
+    else if (op == '^') result = mypow(a, b);
     else {
         printf("Invalid operator.\n");
-        return;
+        return 0;
     }
 
-    printf("Result: %d\n", result);
+    return result;
 }
 
 int main(void) {
@@ -35,11 +24,12 @@ int main(void) {
 
     printf("~~~ Welcome to my calculator ~~~\n\n");
     printf("Enter expression (ex: 1 + 2): ");
-    printf("~~~ Goodbye, See you again ~~~\n\n")
 
-    scanf("%d %c %d", &a, &op, &b);
+    scanf(" %d %c %d", &a, &op, &b);
 
-    calculate(a, op, b);
+    printf("Result = %d\n", calculate(a, op, b));
+
+    printf("~~~ Goodbye, See you again ~~~\n\n");
 
     return 0;
 }
